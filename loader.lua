0f00d8208f1bdb1baa9a79c30dfe7013d3d697f3
marenity <99781024+marenity@users.noreply.github.com>
2022-08-05 07:33:20 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+if game.GameId == 848145103 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/privdq.lua"))()
+elseif game.PlaceId == 292439477 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/privphantom.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/nomenu.lua"))()
+end
