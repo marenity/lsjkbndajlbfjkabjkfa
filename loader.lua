b27b8332b110f96ba2f8e9a4cc8588a7137365d4
marenity <99781024+marenity@users.noreply.github.com>
2022-09-13 04:06:04 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,8 @@ if game.GameId == 848145103 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/privdq.lua"))()
 elseif game.PlaceId == 292439477 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/privphantom.lua"))()
+elseif game.GameId == 1116949753 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/priv_isle.lua"))()    
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/nomenu.lua"))()
 end
