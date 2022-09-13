bd8d6dea18621b9b54d87617c6840019f0c621da
marenity <99781024+marenity@users.noreply.github.com>
2022-09-13 04:58:08 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,11 @@
-if game.GameId == 848145103 then
+if game.GameId == 848145103 then --dungeon
     loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/privdq.lua"))()
-elseif game.PlaceId == 292439477 then
+elseif game.PlaceId == 292439477 then --pf
     loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/privphantom.lua"))()
-elseif game.GameId == 1116949753 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/priv_isle.lua"))()    
+elseif game.GameId == 1116949753 then --isle
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/priv_isle.lua"))()
+elseif game.GameId == 2440500124 then --doors
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/priv_doors.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/marenity/lsjkbndajlbfjkabjkfa/main/nomenu.lua"))()
 end
