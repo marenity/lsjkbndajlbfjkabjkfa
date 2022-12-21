2349205491597bdf0eaab188b02b2d9a3a5a9dc3
marenity <99781024+marenity@users.noreply.github.com>
2022-12-21 03:44:34 -0700

    Update testnewlib.lua

--- testnewlib.lua
+++ testnewlib.lua
@@ -29,9 +29,9 @@ local Library = {
     HudRegistry = {};
 
     FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
-    BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 85, 255);
+    MainColor = Color3.fromRGB(9, 9, 9);
+    BackgroundColor = Color3.fromRGB(255, 9, 9);
+    AccentColor = Color3.fromRGB(15, 15, 15);
     OutlineColor = Color3.fromRGB(50, 50, 50);
 
     Black = Color3.new(0, 0, 0);
@@ -2401,9 +2401,10 @@ function Library:CreateWindow(...)
         Tabs = {};
     };
 
-    local Outer = Library:Create('Frame', {
+    --outermost outline color
+    local Outer = Library:Create('Frame', { 
         AnchorPoint = Config.AnchorPoint,
-        BackgroundColor3 = Color3.new(1, 0, 0);
+        BackgroundColor3 = Color3.new(0, 0, 0);
         BorderSizePixel = 0;
         Position = Config.Position,
         Size = Config.Size,
