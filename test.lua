bd03979c0d8a0fe9602c2a028472be070b77e899
marenity <99781024+marenity@users.noreply.github.com>
2023-01-30 14:39:02 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,10 +8,10 @@ local RenderStepped = RunService.RenderStepped;
 local LocalPlayer = Players.LocalPlayer;
 local Mouse = LocalPlayer:GetMouse();
 
-local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
+--local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
 local ScreenGui = Instance.new('ScreenGui');
-ProtectGui(ScreenGui);
+--ProtectGui(ScreenGui);
 ScreenGui.Name = "LookingForThis"
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
